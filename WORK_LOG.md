6ed31fdc63b8ad8178a0a9a33ce0e3149c72a0b9
kate <kate.little@menloschool.org>
2024-10-30 13:53:22 -0700

    10/30 - 1st commit

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,9 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date   |    Time    |                                                Update |
+|:-------|:----------:|------------------------------------------------------:|
+| Oct 25 | 2:30-3:00  | I started thinking about the problem with Liliana. We |
+| Oct 30 | 12:35-1:45 |                                  Wrote hash function  |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Finder.java
+++ src/Finder.java
@@ -13,11 +13,33 @@ public class Finder {
 
     private static final String INVALID = "INVALID KEY";
 
+
     public Finder() {}
+    static final int RADIX = 256;
+    // Cataldi's prime
+    static final long PRIME = 137438691328;
+    // Need an array
 
     public void buildTable(BufferedReader br, int keyCol, int valCol) throws IOException {
         // TODO: Complete the buildTable() function!
-        br.close();
+
+        while (br.readLine() != null){
+            // Get an array of all data in line
+            String[] line = br.readLine().split(",");
+            // Make hash for key
+            long hash = makeHash(line[keyCol]);
+
+
+        }
+    }
+
+    // Turns key into a hash
+    public long makeHash(String key){
+        long hash = 0;
+        for (int i = 0; i < key.length(); i++){
+            hash = hash * RADIX + key.charAt(i) % PRIME;
+        }
+        return hash;
     }
 
     public String query(String key){
